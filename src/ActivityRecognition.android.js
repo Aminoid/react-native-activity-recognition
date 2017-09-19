75ccfad5915a8cc359be8bcc25ab817b8bf931f0
Kevin Cooper <cooperka92@gmail.com>
2017-09-19 09:36:09 -0400

    Make JS method exports consistent across Android and iOS

--- src/ActivityRecognition.android.js
+++ src/ActivityRecognition.android.js
@@ -1,48 +1,48 @@
 const { DeviceEventEmitter, NativeModules } = require('react-native')
-const { ActivityRecognition } = NativeModules
+const { ActivityRecognition: ActivityRecognitionNative } = NativeModules
 
-ActivityRecognition.subscribe = subscribe
-ActivityRecognition.start = start
-ActivityRecognition.startMocked = startMocked
-ActivityRecognition.stopMocked = stopMocked
-ActivityRecognition.stop = stop
+const ActivityRecognition = {
+  'ANDROID_STILL': ActivityRecognitionNative.ANDROID_STILL,
+  'ANDROID_WALKING': ActivityRecognitionNative.ANDROID_WALKING,
+  'ANDROID_IN_VEHICLE': ActivityRecognitionNative.ANDROID_IN_VEHICLE,
 
-function subscribe(callback) {
-  const subscription = DeviceEventEmitter.addListener('DetectedActivity', detectedActivities => {
-    callback({
-      ...detectedActivities,
-      get sorted() {
-        return Object.keys(detectedActivities)
-          .map(type => ({ type: type, confidence: detectedActivities[type] }))
-          .sort((a, b) => b.confidence - a.confidence)
-      },
+  subscribe(callback) {
+    const subscription = DeviceEventEmitter.addListener('DetectedActivity', detectedActivities => {
+      callback({
+        ...detectedActivities,
+        get sorted() {
+          return Object.keys(detectedActivities)
+            .map(type => ({ type: type, confidence: detectedActivities[type] }))
+            .sort((a, b) => b.confidence - a.confidence)
+        },
+      })
     })
-  })
-  return () => DeviceEventEmitter.removeSubscription(subscription)
-}
+    return () => DeviceEventEmitter.removeSubscription(subscription)
+  },
 
-function start(detectionIntervalMs) {
-  return new Promise((resolve, reject) => {
-    ActivityRecognition.startWithCallback(detectionIntervalMs, resolve, logAndReject.bind(null, reject))
-  });
-}
+  start(detectionIntervalMs) {
+    return new Promise((resolve, reject) => {
+      ActivityRecognitionNative.startWithCallback(detectionIntervalMs, resolve, logAndReject.bind(null, reject))
+    });
+  },
 
-function startMocked(detectionIntervalMs, mockActivityType) {
-  return new Promise((resolve, reject) => {
-    ActivityRecognition.startMockedWithCallback(detectionIntervalMs, mockActivityType, resolve, logAndReject.bind(null, reject))
-  });
-}
+  startMocked(detectionIntervalMs, mockActivityType) {
+    return new Promise((resolve, reject) => {
+      ActivityRecognitionNative.startMockedWithCallback(detectionIntervalMs, mockActivityType, resolve, logAndReject.bind(null, reject))
+    });
+  },
 
-function stopMocked() {
-  return new Promise((resolve, reject) => {
-    ActivityRecognition.stopMockedWithCallback(resolve, logAndReject.bind(null, reject))
-  });
-}
+  stopMocked() {
+    return new Promise((resolve, reject) => {
+      ActivityRecognitionNative.stopMockedWithCallback(resolve, logAndReject.bind(null, reject))
+    });
+  },
 
-function stop() {
-  return new Promise((resolve, reject) => {
-    ActivityRecognition.stopWithCallback(resolve, logAndReject.bind(null, reject))
-  });
+  stop() {
+    return new Promise((resolve, reject) => {
+      ActivityRecognitionNative.stopWithCallback(resolve, logAndReject.bind(null, reject))
+    });
+  },
 }
 
 function logAndReject(reject, errorMsg) {
